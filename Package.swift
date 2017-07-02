a388d31f02a754eb05d320880d1f9130881a60ab
enums <enum@enumsblog.com>
2017-07-02 17:30:42 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Pjango-SQLite",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/enums/Pjango.git", majorVersion: 0),
+        .Package(url: "https://github.com/enums/Pjango.git", majorVersion: 0, minor: 9),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", majorVersion: 2),
     ]
 )
